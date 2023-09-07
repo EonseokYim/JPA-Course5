7be2bc068dd89654e0cf4445bcd676a55ff6ff7a
EonSeok <eonse32@gmail.com>
2023-09-07 15:51:46 +0900

    실무활용 - 스프링 데이터 페이징 활용1 - Querydsl 페이징 연동

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -1,8 +1,12 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
@@ -59,4 +63,76 @@ public class MemberRepositoryImpl implements  MemberRepositoryCustom {
         return ageLoe != null ? member.age.loe(ageLoe) : null;
     }
 
+    @Override
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<MemberTeamDto> results = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        testNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+/**             fetchResults(), fetchCount()는 Deprecated되었다.
+                왜냐하면 해당 메소드는 그냥 단순하게 쿼리의 row를 count하는 구조로 되어있기 때문에 복잡한 쿼리는 count를 구하는데 있어서 시간이 엄청 걸리거나
+                제대로 count를 계산할 수 없는 상황이 발생하기 때문이다.
+                심플한 쿼리는 상관없지만.. 모두 항상 심플하지는 않기에 deprecated 되었다.
+                그래서 직접 count쿼리를 짜야한다. 아래에 searchPageComplex()를 참고
+                */
+
+        List<MemberTeamDto> content = results.getResults();
+        long total = results.getTotal();
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+        List<MemberTeamDto> results = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        testNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+
+        //count query를 최적화할 수 있는 방법을 모색해서 직접 카운트 쿼리를 작성한다.
+        //데이터가 별로없으면 그냥 시간들이지 말고 fetchResults사용하고 몇 천만 건 있으면 count 쿼리를 최적화하면 성능 향상 도움에 크다. ex. 카운트쿼리를 먼저구한다음에 만약에 0이면 컨텐츠 쿼리를 실행하지 않는다던가..등등도 도움이 된다.
+        Long total = queryFactory
+                .select(member.id.count())
+                .from(member)
+                .leftJoin(member.team, team) //count 쿼리를 만드는데 join할 필요가 있을까? 상황에 따라서는 조인이 없어도 정확히 count를 계산할 수 있다.
+                .where(usernameEq(condition.getUsername()),
+                        testNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .fetchFirst();
+
+        List<MemberTeamDto> content = results.getResults();
+
+        return new PageImpl<>(content, pageable, total);
+    }
 }
--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
@@ -73,5 +75,18 @@ class MemberRepositoryTest {
 
         em.flush();
         em.clear();
+    }
+
+    @Test
+    public void searchPageSimpleTest() {
+        dataSets();
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        PageRequest pageRequest = PageRequest.of(0, 3);
+
+        Page<MemberTeamDto> result = memberRepository.searchPageSimple(condition, pageRequest);
+
+        assertThat(result.getSize()).isEqualTo(3);
+        assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
     }
 }
